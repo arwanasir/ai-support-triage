e3710088b9a2ff179a2c625ecd1fe4db50e4ac6a
arwanasir <arwanasir04@gmail.com>
2026-08-21 05:07:59 -0700

    feat:added detail page for the tickets

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -14,11 +14,42 @@ export interface Ticket {
 
 }
 
+export interface ReplyPayload {
+    ticketId: string;
+    response: string;
+    action: 'APPROVE' | 'REJECT' | 'SAVE_DRAFT'
+}
+
 export async function fetchTickets(): Promise<Ticket[]> {
     const res = await fetch(`${API_URL}/tickets`);
     if (!res.ok) {
         throw new Error("failed to fetch the tickets from backend API");
     }
 
+    return res.json();
+}
+
+export async function fetchTicketsById(id: string): Promise<Ticket> {
+    const res = await fetch(`${API_URL}/tickets/${id}`)
+
+    if (!res) {
+        throw new Error(`failed to fetch ticket ${id}`)
+    }
+    return res.json();
+}
+export async function sendTicketReply(payload: ReplyPayload): Promise<{ success: boolean; message?: string }> {
+    const res = await fetch(`${API_URL}/tickets/${payload.ticketId}/reply`, {
+        method: 'POST',
+        headers: {
+            'content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            response: payload.response,
+            action: payload.action
+        })
+    });
+    if (!res.ok) {
+        throw new Error('Failed to submit ticket reply')
+    }
     return res.json();
 }
